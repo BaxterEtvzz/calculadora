77d66ec1f826f1a2082ef044813e21fb597f443d
Oustin Estevez <oustinestevez@MacBook-Air-de-Oustin.local>
2019-02-08 20:52:42 -0600

    calculadora de operaciones

---
+++ index.js
@@ -0,0 +1,6 @@
+const math = require ('./math');
+
+console.log("el resultado de la suma es: " + math.suma(10,20));
+console.log("el resultado de la resta es: " + math.resta(80,20));
+console.log("el resultado de la multiplicacion es: " + math.multiplicacion(5,5));
+console.log("el resultado de la division es: " + math.division(100, 5));
